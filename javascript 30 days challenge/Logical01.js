4bc644b0ec8f6e06a1bbab9a1791c7816a27cf6f
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-11-18 15:28:34 +0500

    Delete javascript 30 days challenge/Logical01.js

--- javascript 30 days challenge/Logical01.js
+++
@@ -1,8 +0,0 @@
-// User Logged In = true
-// User isAdmin = false
-// Check karo user admin panel open kar sakta hai ya nahi (&& use karo)
-
-let logged = true 
-let isAdmin = false 
-let adminPanel = logged && isAdmin
-console.log(adminPanel);
