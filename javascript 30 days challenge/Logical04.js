69a4c4433bc50161bdb589d38a6aa0ae6eeb0dfe
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-11-18 15:29:43 +0500

    Delete javascript 30 days challenge/Logical04.js

--- javascript 30 days challenge/Logical04.js
+++
@@ -1,6 +0,0 @@
-// isPremium = false
-// Use ! operator se check karo user premium nahi hai
-
-let isPremium = false;
-let isNotPremium = !isPremium ;
-console.log("User premium nahi hai?", isNotPremium);
