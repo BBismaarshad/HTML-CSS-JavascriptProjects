7cecd3dec92c6d192f5d63a4bc9cf8684aeb5543
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-11-18 15:26:39 +0500

    Delete javascript 30 days challenge/Assignment02.js

--- javascript 30 days challenge/Assignment02.js
+++
@@ -1,8 +0,0 @@
-// Cart total = 1200
-// User ne ek product remove kiya jiska price 300 tha
-// -= use karke new total nikalo.
-
-let cartTotal = 1200;
-cartTotal -= 200;
-
-console.log(cartTotal );
