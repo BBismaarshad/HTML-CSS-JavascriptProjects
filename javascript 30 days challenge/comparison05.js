28dbab704ee2216acd0538babf33302e84d97915
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-11-18 15:27:36 +0500

    Delete javascript 30 days challenge/comparison05.js

--- javascript 30 days challenge/comparison05.js
+++
@@ -1,5 +0,0 @@
-// User password = "1234"
-// Check karo kya password "abcd" nahi hai (!=).
-let password = "1234";
-let isNotAbcd = password != "abcd";
-console.log("Kya password 'abcd' nahi hai?", isNotAbcd);
