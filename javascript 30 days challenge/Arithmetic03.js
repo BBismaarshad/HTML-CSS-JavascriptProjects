5203b9352a70504a1330be14faaa4bae70701622
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-11-18 15:25:31 +0500

    Delete javascript 30 days challenge/Arithmetic03.js

--- javascript 30 days challenge/Arithmetic03.js
+++
@@ -1,7 +0,0 @@
-// Ek rectangle ki width = 8 aur height = 5
-// Area find karo (width * height).
-
-let width = 8;
-let height = 5;
-let area = width * height;
-console.log("Area of Rectangle:", area);
