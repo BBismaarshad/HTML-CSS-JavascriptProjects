1d5f19fa8a0a3e8c73ea1101234f4cdb2b44abbc
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-11-18 11:58:52 +0500

    Add files via upload

---
+++ javascript 30 days challenge/Logical01.js
@@ -0,0 +1,8 @@
+// User Logged In = true
+// User isAdmin = false
+// Check karo user admin panel open kar sakta hai ya nahi (&& use karo)
+
+let logged = true 
+let isAdmin = false 
+let adminPanel = logged && isAdmin
+console.log(adminPanel);
---
+++ javascript 30 days challenge/comparison04.js
@@ -0,0 +1,6 @@
+// Price = 999
+// Check karo price 1000 se kam hai ya nahi.
+
+let price = 999;
+let isLessThan1000 = price < 1000;
+console.log("Kya price 1000 se kam hai?", isLessThan1000);
