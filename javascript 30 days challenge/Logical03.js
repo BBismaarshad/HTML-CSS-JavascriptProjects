783e193ea93ebf21e879917c42da9836ff85047e
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-11-18 15:29:26 +0500

    Delete javascript 30 days challenge/Logical03.js

--- javascript 30 days challenge/Logical03.js
+++
@@ -1,8 +0,0 @@
-// hasEmail = true
-// hasPassword = false
-// Check karo: User form submit kar sakta hai ya nahi (both required using &&)
-
-let hasEmail = true;
-let hasPassword = false;
-let submitFrom = hasEmail && hasPassword;
-console.log(submitFrom);
